582a6b3a4bfb698bd775fa858b6bb7e90e437bd7
Diego Antunes Costa <diegoantunescosta@gmail.com>
2022-05-17 23:59:39 -0300

    update

--- fresquinha-java/src/main/java/com/example/fresquinhajava/VendedorRepository.java
+++ fresquinha-java/src/main/java/com/example/fresquinhajava/VendedorRepository.java
@@ -3,7 +3,7 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "vendedor", path = "vendedores")
+@RepositoryRestResource(collectionResourceRel = "vendedos", path = "vendedores")
 public interface VendedorRepository extends MongoRepository<Vendedor,String>{
 
     List<Vendedor> findByNome(String nome);
