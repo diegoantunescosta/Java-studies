8398fbd9916e8da1ceec9df0930263a8a0eb2601
Diego Antunes Costa <diegoantunescosta@gmail.com>
2022-05-30 13:44:10 -0300

    corrigindo

--- fresquinha_mysql/src/main/java/com/example/fresquinha_mysql/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fresquinha_mysql;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoriaRepository extends CrudRepository<Categoria, Integer>{
-
-    
-}
